10d2c89c8165469b69b2fac3491869fd00b3a7e6
Phil <35858888+RxParalyze@users.noreply.github.com>
2021-03-15 16:08:04 -0400

    stacks.py progress

--- stacks.py
+++ stacks.py
@@ -1,6 +1,7 @@
 import json
 import urllib.request
 import re
+import tensorflow as tf
 
 with open('items_result.json') as f:
     data = json.load(f)
@@ -22,29 +23,34 @@ typeDict = [
     "Critical Damage",
     "Health",
     "HP5",
-    "Magical Defense",
     "Magical Lifesteal",
+    "Magical Protection",
     "Magical Power",
     "Mana",
     "Movement Speed",
     "MP5",
     "Penetration Chance",
     "Penetration Power",
-    "Physical Defense",
     "Physical Lifesteal",
-    "Physical Power"
+    "Physical Power",
+    "Physical Protection"
 ]
 
 for sourceItem in sourceItems:
-    for phrase in stacksDict:
-        if phrase in sourceItem['passive']:
-            for category in typeDict:
-                if category in sourceItem['passive']:
-                    #amount = re.findall(r'\d+', basicAttackFlatIncrease.group())
-                    amount = re.findall(r'\d+')
-                    sourceItem['stacks']['stacks'][category]['amount'] = amount
-                    break
-            break
+    if "passive" in sourceItem:
+        for phrase in stacksDict:
+            if phrase in sourceItem['passive'].lower():
+                print(sourceItem['name'] + ": {}".format(phrase))
+                for category in typeDict:
+                    print(sourceItem['name'] + ": {}".format(category))
+                    if category in sourceItem['passive'].lower():
+                        print(sourceItem['name'] + ": {}".format(category))
+                        #amount = re.findall(r'\d+', basicAttackFlatIncrease.group())
+                        amount = re.findall(r'\d+')
+                        sourceItem['stacks']['stacks'][("{}".format(category))]['amount'] = amount
+                        continue
+                continue
+    items.append(sourceItem)
 
 with open('items_result.json', 'w') as json_file:
     json.dump(items, json_file, indent='\t', sort_keys=True)
